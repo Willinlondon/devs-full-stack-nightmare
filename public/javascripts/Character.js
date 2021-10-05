106607118fbd8a591234a4f074d8121b13d9d45a
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-05 16:36:16 +0100

    WD & AS: Add first test to character class

--- public/javascripts/Character.js
+++ public/javascripts/Character.js
@@ -3,9 +3,12 @@ class Character {
     this.name = name;
     this.health = 100;
     this.location = [0,0];
+    console.log("Made a character!")
   }
 
   attack() {
 
   }
 }
+
+module.exports = { Character: Character }
--- spec/CharacterSpec.js
+++ spec/CharacterSpec.js
@@ -1,8 +1,8 @@
 describe('Character', () => {
-  const Character = require('../public/javascripts/Character');
+  const { Character } = require('../public/javascripts/Character');
 
   beforeEach(() => {
-    const player = new Character("Will");
+    player = new Character("Will");
   });
 
   it('has a default health of 100', () => {
