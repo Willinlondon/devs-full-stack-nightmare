42a3b21e91d432b01098b7523c15fd5327a59b2f
Fiona Apps <wikipanyd@gmail.com>
2021-10-06 17:19:57 +0100

    adding lots and lots of notes

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -3,4 +3,27 @@ class Game {
     this.map = map.createMap();
     this.player = player;
   }
+
+  
+
+  // mapPosition() 
+  // player.move('direction') - this will move the player
+  // game.round(?)turn(?)othersynonym(?) - this will determine the state
+  // game.enum - this will trigger a battle or no?
+  
+  // game.over - if the player loses their battle it's game over
+  
+
+
+  // This wants to have the following methods:
+
+  // Every time the CHARACTER takes a step
+  // A dice is rolled to see what STATE it is
+  // And IF battle THEN move to BATTLE
+
+  // So the methods here are:
+
+  // 
+
+
 }
