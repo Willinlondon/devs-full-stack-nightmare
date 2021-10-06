67868389daf58eb55c5b72366c957a4682117ce2
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-06 14:00:43 +0100

    AS: Add rough and ready map display to sketch

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,12 +1,28 @@
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+
+  const map = new Map;
+  map.createHardCodedMap();
+
+  fill(0);
+
+  console.log(map.mapArray);
+  map.mapArray.forEach((y, y_index) => {
+    y.forEach((x, x_index) => {
+      if (x == 1) {
+        fill(150, 200, 150)
+        rect(x_index * 200, y_index * 200, 200)
+      }
+    })
+  })
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  // if (mouseIsPressed) {
+  //   fill(0);
+  // } else {
+  //   fill(255);
+  // }
+  // ellipse(mouseX, mouseY, 80, 80);
 }
