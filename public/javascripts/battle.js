844feb853560e8f457ed1b5bc99068168350e56f
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-06 12:35:05 +0100

    AS: Lint specs

--- public/javascripts/battle.js
+++ public/javascripts/battle.js
@@ -1,16 +1,16 @@
 class Battle {
-    constructor(player1 = "Captain Planet", player2 = "Skeletor") {
-        this.player1 = player1;
-        this.player2 = player2;
-    }
+  constructor(player1 = "Captain Planet", player2 = "Skeletor") {
+    this.player1 = player1;
+    this.player2 = player2;
+  }
 
-    winner(player1Roll, player2Roll) {
-        if (player1Roll > player2Roll) {
-            return 'Player1 Wins!';
-        } 
-        else if (player1Roll < player2Roll){
-          return 'Player 2 Wins!'
-        } else 
-        return 'Boo!! Draw Try Harder!'
-    }
+  winner(player1Roll, player2Roll) {
+    if (player1Roll > player2Roll) {
+      return 'Player1 Wins!';
+    } 
+    else if (player1Roll < player2Roll){
+      return 'Player 2 Wins!'
+    } else 
+    return 'Boo!! Draw Try Harder!'
+  }
 }
--- spec/battleSpec.js
+++ spec/battleSpec.js
@@ -1,21 +1,21 @@
 describe('Battle', () => {  
   describe("when battle has been called with default values", function() {
-  let player1;
-  let player2;
+    let player1;
+    let player2;
 
-  beforeEach(function() {
-    battle = new Battle(player1, player2);
-  });
+    beforeEach(function() {
+      battle = new Battle(player1, player2);
+    });
 
-  it('inititalizes with player 1', () => {
-    expect(battle.player1).toEqual('Captain Planet')
+    it('inititalizes with player 1', () => {
+      expect(battle.player1).toEqual('Captain Planet')
+    })
+
+    it('inititalizes with player 2', () => {
+      expect(battle.player2).toEqual('Skeletor')
+    })
   })
-
-  it('inititalizes with player 2', () => {
-    expect(battle.player2).toEqual('Skeletor')
-  })
-  } )
-  
+    
   it('inititalizes with player 1 and allows user to input own variable', () => {
     // This also works if you pass the string directly into the constructor
     let player1 = 'Wolverine'
@@ -25,33 +25,30 @@ describe('Battle', () => {
   })
 
   describe("when a user is playing the game", function() {
+    let player1;
+    let player2;
+    
+    beforeEach(function() {
+      battle = new Battle(player1, player2);
+    });
+    
+    it('lets player 1 win if their roll is higher', () => {
+      let player1Roll = 5
+      let player2Roll = 2
+      expect(battle.winner(player1Roll, player2Roll)).toEqual('Player1 Wins!')
+    })
 
-  let player1;
-  let player2;
-  
-  beforeEach(function() {
-    battle = new Battle(player1, player2);
-  });
-  
-  it('lets player 1 win if their roll is higher', () => {
-    let player1Roll = 5
-    let player2Roll = 2
-    expect(battle.winner(player1Roll, player2Roll)).toEqual('Player1 Wins!')
+    it('lets player 2 win if they roll higher', () => {
+      let player1Roll = 6
+      let player2Roll = 7
+      expect(battle.winner(player1Roll, player2Roll)).toEqual('Player 2 Wins!')
+    })
+
+    it('allows for the characters to draw', () => {
+      let player1Roll = 1
+      let player2Roll = 1
+      expect(battle.winner(player1Roll, player2Roll)).toEqual('Boo!! Draw Try Harder!')
+    })
   })
-
-  it('lets player 2 win if they roll higher', () => {
-    let player1Roll = 6
-    let player2Roll = 7
-    expect(battle.winner(player1Roll, player2Roll)).toEqual('Player 2 Wins!')
-  })
-
-  it('allows for the characters to draw', () => {
-    let player1Roll = 1
-    let player2Roll = 1
-    expect(battle.winner(player1Roll, player2Roll)).toEqual('Boo!! Draw Try Harder!')
-  })
-
-  })
-
 })
 
