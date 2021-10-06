b2f7f96f3c0ae5e22509aa80262c16cd98c38e28
Fiona Apps <wikipanyd@gmail.com>
2021-10-06 12:00:01 +0100

    Adding comments regarding placeholder file

---
+++ public/javascripts/player.js
@@ -0,0 +1,8 @@
+// This is a placeholder file
+// It's here in case we want to extend the character
+// out to another class. We chose to keep the movement
+// within the Character.js class until such time as 
+// refactoring is required
+
+// However on our design docs it says we should be
+// placing movement methods in here.
