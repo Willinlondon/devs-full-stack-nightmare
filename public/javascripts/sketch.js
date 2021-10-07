d7b7ef5322947f9d4669c121b99dda6436c16b5a
William Day <williamjday11@gmail.com>
2021-10-07 15:43:18 +0100

    SA & WD: Add sources to index.ejs for all active classes

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,5 +1,5 @@
 const game = new Game
-
+console.log(game.player.location)
 function setup() {
   canvas = createCanvas(750, 750);
   canvas.parent("play-area")
@@ -8,5 +8,5 @@ function setup() {
 function draw() {
   background(0)
   fill(155)
-  rect(0,0,75)
+  rect(game.player.location[0],game.player.location[1],75)
 }
