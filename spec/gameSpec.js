84ae74a5e96acf48269b924ff1a5a3ae56b20204
William Day <williamjday11@gmail.com>
2021-10-07 11:57:58 +0100

    AS, FA & WD: Fix Jasmine for Battle class

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,10 +1,11 @@
 describe('Game', () => {
 
-  beforeEach(() => {
-    game = new Game;
-  });
-
   describe('When a new game is created', () => {
+
+    beforeEach(() => {
+      game = new Game;
+    });
+
     it('has a map', () => {
       expect(game.map).toBeInstanceOf(Array);
     });
