55562d0deda9216a429bcdd09607d6367616d16c
Fiona Apps <wikipanyd@gmail.com>
2021-10-07 10:48:39 +0100

    adding main game loop v0.5

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,16 +2,40 @@ class Game {
   constructor(map = new Map, player = new Character) {
     this.map = map.createMap();
     this.player = player;
+    this.state = inGame;
   }
 
   // while state = in game
-  // player.playerMove()
-  // roll dice to see if battle is commenced
-  // if battle commenced, run battle
-  //    // if player wins, back to square one
-  //    // if player loses, state = game over
-  // else go back to square one
+  while (this.state === inGame) {
+    // player.playerMove()
+    player.playerMove()
+    // roll dice to see if battle is commenced
+    this._checkState(_encounterRoll)
+    // if battle commenced, run battle
+    if (this.state === battle){
+      battle = new Battle
+      battle.winner((Math.random() * 20), (Math.random() * 20))
+      // if player loses, state = game over
+      if('Player2 Wins!'){
+        this.state === gameOver
+      }
+      // else go back to square one
+    }
+  }
 
+  _checkState(_encounterRoll()) {
+    if (_encounterRoll > 80){ this.state = battle};
+    if (_encounterRoll <= 80)( this.state = inGame);
+  }
+
+  _encounterRoll() {
+    return Math.random() * 100
+  }
+
+
+
+
+  // -----------
   // mapPosition() 
   // player.move('direction') - this will move the player
   // game.round(?)turn(?)othersynonym(?) - this will determine the state
