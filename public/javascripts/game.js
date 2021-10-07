6e4fd4f9cc267f3781ddd21c081b61b08fdb1c6a
Fiona Apps <wikipanyd@gmail.com>
2021-10-07 17:07:43 +0100

    Severe refactoring of main methods

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -5,50 +5,31 @@ class Game {
 		this.state = "inGame";
 	}
 	
-
 	//player takes a move
-	//it rolls for random encounter
-	//if you win, yay, you can move again
-	//if you lose, boo, gameOver
-	
-
-
-	play() {
-		// while state = in game
-		while (this.state === "inGame") {
-			// player.playerMove()
-			// Have added a manual input before we incorporate Amelia's P5 movement stuff
-			this.player.move(this._playerInput());
-			console.log(this.player.location);
-			console.log('the player has moved');
-			// roll dice to see if battle is commenced
-			this._checkState(this._encounterRoll());
-			console.log('the encounter roll has happened');
-			console.log(this.state);
-			// if battle commenced, run battle
-			if (this.state === "battle") {
-				console.log('BATTLE!');
-				let battle = new Battle();
-				console.log(battle)
-				battle.winner(Math.random() * 20, Math.random() * 20);
-				// if player loses, state = game over
-				if ("Player2 Wins!") {
-					this.state = "gameOver"
-					return ;
-				}
-				// else go back to square one
-				else {
-					this.state = "inGame";
-				}
-			}
+	playerAction(direction = _playerInput()){
+		this.player.move(direction);
+		this._checkState(this._encounterRoll());
+		if (this.state === "battle") {
+			this._playerFight()
 		}
 	}
 
+	_playerFight(){
+		let battle = new Battle();
+		battle.winner(Math.random() * 20, Math.random() * 20);
+		if ("Player2 Wins!") {
+			console.log('PLAYER 2 WON BB')
+			this.state = "gameOver"
+			return ;
+		}
+		else {this.state = "inGame"};
+	}
+
 	_playerInput(){
-		if (keyCode === 87) {'up'};
-		if (keyCode === 83) {'down'};
-		if (keyCode === 65) {'left'};
-		if (keyCode === 68) {'right'};
+		if (keyCode === 87) {"up"};
+		if (keyCode === 83) {"down"};
+		if (keyCode === 65) {"left"};
+		if (keyCode === 68) {"right"};
 }
 
 	_checkState(_encounterRoll) {
@@ -64,23 +45,4 @@ class Game {
 		return Math.random() * 100;
 	}
 
-
-
-	// -----------
-	// mapPosition()
-	// player.move('direction') - this will move the player
-	// game.round(?)turn(?)othersynonym(?) - this will determine the state
-	// game.enum - this will trigger a battle or no?
-
-	// game.over - if the player loses their battle it's game over
-
-	// This wants to have the following methods:
-
-	// Every time the CHARACTER takes a step
-	// A dice is rolled to see what STATE it is
-	// And IF battle THEN move to BATTLE
-
-	// So the methods here are:
-
-	//
 }
