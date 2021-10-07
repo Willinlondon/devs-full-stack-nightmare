ffbeebc60324584a5f3f67742324c064daa6ed98
Ahsan <rasul.ahsan@gmail.com>
2021-10-07 15:33:58 +0100

    FA & AR: Add tests for game loop

--- public/javascripts/character.js
+++ public/javascripts/character.js
@@ -1,44 +1,49 @@
 class Character {
-  constructor(name) {
-    this.name = name;
-    this.health = 100;
-    this.location = [0,0];
-  }
+	constructor(name) {
+		this.name = name;
+		this.health = 100;
+		this.location = [0, 0];
+	}
 
-  attack() {
-    return Math.floor(Math.random() * 20)
-  }
+	attack() {
+		return Math.floor(Math.random() * 20);
+	}
 
-  move(direction) {
-    if (direction == 'right') {this._moveRight()};
-    if (direction == 'left') {this._moveLeft()};
-    if (direction == 'up') {this._moveUp()};
-    if (direction == 'down') {this._moveDown()};
-  }
+	move(direction) {
+		if (direction == "right") {
+			this._moveRight();
+		}
+		if (direction == "left") {
+			this._moveLeft();
+		}
+		if (direction == "up") {
+			this._moveUp();
+		}
+		if (direction == "down") {
+			this._moveDown();
+		}
+	}
 
-  // Directions are here as private methods
-  
-  _moveRight() {
-    this.location[0] += 1 
-  }
-  
-  _moveLeft() {
-    this.location[0] -= 1 
-  }
+	// Directions are here as private methods
 
-  _moveUp() {
-    this.location[1] += 1 
-  }
+	_moveRight() {
+		this.location[0] += 1;
+	}
 
-  _moveDown() {
-    this.location[1] -= 1 
-  }
+	_moveLeft() {
+		this.location[0] -= 1;
+	}
 
+	_moveUp() {
+		this.location[1] += 1;
+	}
 
-  // - Jasmine says this is undefined
-  // move_right = function() {
-  //   this.location[0] += 1
-  // }
+	_moveDown() {
+		this.location[1] -= 1;
+	}
 
+	// - Jasmine says this is undefined
+	// move_right = function() {
+	//   this.location[0] += 1
+	// }
 }
-
--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,33 +1,49 @@
-describe('Game', () => {
+describe("Game", () => {
+	describe("When a new game is created", () => {
+		beforeEach(() => {
+			game = new Game();
+		});
 
-  describe('When a new game is created', () => {
+		it("creates a map upon new game", () => {
+			expect(game.map).toBeInstanceOf(Array);
+		});
 
-    beforeEach(() => {
-      game = new Game;
-    });
+		it("creates a player upon new game", () => {
+			expect(game.player).toBeInstanceOf(Character);
+		});
 
-    it('creates a map upon new game', () => {
-      expect(game.map).toBeInstanceOf(Array);
-    });
+		it("creates a default inGame state upon new game", () => {
+			expect(game.state).toEqual("inGame");
+		});
+	});
 
-    it('creates a player upon new game', () => {
-      expect(game.player).toBeInstanceOf(Character);
-    });
+	describe("checks the random battle trigger", () => {
+		beforeEach(() => {
+			game = new Game();
+		});
 
-    it('creates a default inGame state upon new game', () => {
-      expect(game.state).toEqual('inGame');
-    });
+		it("checks the range of the encounter roll", () => {
+			expect(game._encounterRoll()).toBeLessThanOrEqual(100);
+		});
 
-  });
+		it("can change this.state ", () => {
+			game._checkState(81);
+			expect(game.state).toEqual("battle");
+		});
+	});
 
-  describe('checks the random battle trigger', () => {
+	describe("it checks the game loop", () => {
+		beforeEach(() => {
+			game = new Game();
+		});
 
-    beforeEach(() => {
-      game = new Game;
-    });
-
-    it('checks the range of the encounter roll', () => {
-      expect(game._encounterRoll()).toBeLessThanOrEqual(100);
-    });
-  })
+		it("can allow player to move", () => {
+			game.play().player.move = jasmine.createSpy();
+			// spyOn(game.player, "move");
+			game.play().player.move();
+			game.play();
+			// console.log(game.player.move());
+			expect(game.play().player.move()).toHaveBeenCalled();
+		});
+	});
 });
