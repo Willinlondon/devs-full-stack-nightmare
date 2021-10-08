30cef46871bc1039a5a8e52f40cbe2ff1dd026a9
Ahsan <rasul.ahsan@gmail.com>
2021-10-08 17:45:20 +0100

    DR & AR: Modified character to have a valid starting position based on the map

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,40 +1,45 @@
 class Game {
 	constructor(map = new Map(), player = new Character()) {
-		this.map = map.createMap();
+		this.gameMap = map;
+		this.map = this.gameMap.createMap();
 		this.player = player;
+		this.player.startLocation(
+			this.gameMap.startingColumn,
+			this.gameMap.startingRow
+		);
 		this.state = "inGame";
 		this.cells = this._generateCells();
 	}
-	
+
 	//player takes a move
+
 	playerAction(direction, amount) {
 		let playerX = this.player.location[0];
 		let playerY = this.player.location[1];
 		let legalMove = true;
 
 		switch (direction) {
-		  case 'right':
-		  	legalMove = !this._cellAt(playerX + amount, playerY).isWall();
-		    break;
-		  case 'left':
-		  	legalMove = !this._cellAt(playerX - amount, playerY).isWall();
-		  	break;
-		  case 'up':
-		  	legalMove = !this._cellAt(playerX, playerY - amount).isWall();
-		    break;
-		  case 'down':
-		  	legalMove = !this._cellAt(playerX, playerY + amount).isWall();
-		    break;
+			case "right":
+				legalMove = !this._cellAt(playerX + amount, playerY).isWall();
+				break;
+			case "left":
+				legalMove = !this._cellAt(playerX - amount, playerY).isWall();
+				break;
+			case "up":
+				legalMove = !this._cellAt(playerX, playerY - amount).isWall();
+				break;
+			case "down":
+				legalMove = !this._cellAt(playerX, playerY + amount).isWall();
+				break;
 		}
-		
-		if(legalMove) {
+
+		if (legalMove) {
 			this.player.move(direction, amount);
 			this._checkState(this._encounterRoll());
 			if (this.state === "battle") {
-				this._playerFight()
+				this._playerFight();
 			}
 		}
-
 	}
 
 	showMap() {
@@ -42,7 +47,7 @@ class Game {
 			y.forEach((x, x_index) => {
 				let currentCell = this._cellAt(x_index * 75, y_index * 75);
 
-				if(currentCell.isWall()) {
+				if (currentCell.isWall()) {
 					fill(150, 50, 150);
 					rect(currentCell.x, currentCell.y, 75);
 				}
@@ -51,7 +56,7 @@ class Game {
 	}
 
 	_cellAt(x, y) {
-		return this.cells.find(cell => (cell.x == x && cell.y == y));
+		return this.cells.find((cell) => cell.x == x && cell.y == y);
 	}
 
 	_generateCells() {
@@ -59,7 +64,7 @@ class Game {
 
 		this.map.forEach((y, y_index) => {
 			y.forEach((x, x_index) => {
-				let wall = (x == 1) ? true : false
+				let wall = x == 1 ? true : false;
 
 				cellArray.push(new Cell(x_index * 75, y_index * 75, wall));
 			});
@@ -68,7 +73,7 @@ class Game {
 		return cellArray;
 	}
 
-	_playerFight(){
+	_playerFight() {
 		let battle = new Battle();
 		battle.winner(Math.random() * 20, Math.random() * 20);
 		this.state = "inGame";
@@ -86,5 +91,4 @@ class Game {
 	_encounterRoll() {
 		return Math.random() * 100;
 	}
-
 }
