5e8f39f517c4c91c47e5aca9e2f27e3ee8fef9c8
Fiona Apps <wikipanyd@gmail.com>
2021-10-08 10:14:25 +0100

    Updating method called from sketch file

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -13,8 +13,8 @@ function draw() {
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW || keyCode === 65) {game.player.move('left', 75)};
-  if (keyCode === RIGHT_ARROW || keyCode === 68) {game.player.move('right', 75)};
-  if (keyCode === UP_ARROW || keyCode === 87) {game.player.move('up', 75)};
-  if (keyCode === DOWN_ARROW || keyCode === 83) {game.player.move('down', 75)};
+  if (keyCode === LEFT_ARROW || keyCode === 65) {game.playerAction('left', 75)};
+  if (keyCode === RIGHT_ARROW || keyCode === 68) {game.playerAction('right', 75)};
+  if (keyCode === UP_ARROW || keyCode === 87) {game.playerAction('up', 75)};
+  if (keyCode === DOWN_ARROW || keyCode === 83) {game.playerAction('down', 75)};
 }
