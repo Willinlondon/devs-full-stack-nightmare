dc7e7a65a8c475f4f6ce6490745ee6650c49780b
Fiona Apps <wikipanyd@gmail.com>
2021-10-08 10:16:07 +0100

    updating to synch with sketch

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -6,8 +6,8 @@ class Game {
 	}
 	
 	//player takes a move
-	playerAction(direction = _playerInput()){
-		this.player.move(direction);
+	playerAction(direction = _playerInput(), amount){
+		this.player.move(direction, amount);
 		this._checkState(this._encounterRoll());
 		if (this.state === "battle") {
 			this._playerFight()
