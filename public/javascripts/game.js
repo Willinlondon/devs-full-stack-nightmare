b16acdb53ef993610063447b2bfbf5aaaacbe4a4
Fiona Apps <wikipanyd@gmail.com>
2021-10-08 10:40:09 +0100

    FA fixing the battle outcome

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -17,12 +17,7 @@ class Game {
 	_playerFight(){
 		let battle = new Battle();
 		battle.winner(Math.random() * 20, Math.random() * 20);
-		if ('Player 2 Wins!') {
-			console.log('PLAYER 2 WON BB')
-			this.state = "gameOver"
-			{ return } ;
-		}
-		else {this.state = "inGame"};
+		this.state = "inGame";
 	}
 
 	_checkState(_encounterRoll) {
