bb9e5cfec183d2e1335de188e3b37f6c1ecfc1a0
William Day <williamjday11@gmail.com>
2021-10-11 16:56:55 +0100

    WD: Add logic for dodge on basic attack

--- public/javascripts/battle.js
+++ public/javascripts/battle.js
@@ -7,17 +7,32 @@ class Battle {
   takeTurn() {
     let player1Attack = Math.floor(Math.random() * Config.baseAttack);
     let player2Attack = Math.floor(Math.random() * Config.baseAttack);
-
-    switch (true) {
-      case player1Attack >= player2Attack:
-        this.player2.takeHit(player1Attack);
-      break;
-      case player2Attack > player1Attack:
-        this.player1.takeHit(player2Attack);
-      break;
-      default:
-      break;
+   
+    if (Math.random() * 100 > Config.dodgeChance) {
+      console.log("Player attack", player1Attack)
+      this.player2.takeHit(player1Attack);
     }
+    else {
+      console.log("Jasmine dodged the Player's attack")
+    }
+
+    if (Math.random() * 100 > Config.dodgeChance) {
+      console.log("Jasmine attack", player2Attack)
+      this.player1.takeHit(player2Attack);
+    }
+    else {
+      console.log("Player dodged Jasmine's attack")
+    }
+    // switch (true) {
+    //   case player1Attack >= player2Attack:
+    //     this.player2.takeHit(player1Attack);
+    //   break;
+    //   case player2Attack > player1Attack:
+    //     this.player1.takeHit(player2Attack);
+    //   break;
+    //   default:
+    //   break;
+    // }
   }
 
   over() {
