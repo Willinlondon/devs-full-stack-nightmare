2b7f5fa2c383adf050908574937b5f7f86849e79
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-12 18:08:30 +0100

    AR & AS: Fixed exit location bug and refactored

--- public/javascripts/cell.js
+++ public/javascripts/cell.js
@@ -16,28 +16,31 @@ class Cell {
   calculateExits() {
     this.exits.west = this._freeCell([this.x - Config.cellSize, this.y]);
     this.exits.east = this._freeCell([this.x + Config.cellSize, this.y]);
-    this.exits.north = this._freeCell([this.y - Config.cellSize, this.x]);
-    this.exits.south = this._freeCell([this.y + Config.cellSize, this.x]);
-    this.exits.northwest = this._freeCell([this.x - Config.cellSize, this.y - Config.cellSize]);
-    this.exits.southwest = this._freeCell([this.x - Config.cellSize, this.y + Config.cellSize]);
-    this.exits.northeast = this._freeCell([this.x + Config.cellSize, this.y - Config.cellSize]);
-    this.exits.southeast = this._freeCell([this.x + Config.cellSize, this.y + Config.cellSize]);
+    this.exits.north = this._freeCell([this.x, this.y - Config.cellSize]);
+    this.exits.south = this._freeCell([this.x, this.y + Config.cellSize]);
+
+    // ANYONE introducing these vars, feel free, but TRIPLE CHECK THE MATHS!!!!!
+    // this.exits.northwest = this._freeCell([this.x - Config.cellSize, this.y - Config.cellSize]);
+    // this.exits.southwest = this._freeCell([this.x - Config.cellSize, this.y + Config.cellSize]);
+    // this.exits.northeast = this._freeCell([this.x + Config.cellSize, this.y - Config.cellSize]);
+    // this.exits.southeast = this._freeCell([this.x + Config.cellSize, this.y + Config.cellSize]);
   }
 
   _freeCell(position) {
     let x = position[0];
     let y = position[1];
+    let cell = this._cellAt(position);
 
-    let result = Cell.all.some((cell) => {
-      return cell.x === x
-      && cell.y === y
-      && cell.x >= 0
-      && cell.y >= 0
-      && cell.x < Config.canvasWidth
-      && cell.y < Config.canvasHeight
-      && !cell.isWall();
-    })
-    
+    if (cell != undefined) {
+      return !cell.wall
+    } else {
+      return false
+    }
+  }
+
+  _cellAt(position) {
+    const result = Cell.all.find(cell => cell.x === position[0] && cell.y === position[1])
+
     return result;
   }
 }
