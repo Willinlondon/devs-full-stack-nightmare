eafc40df523578ba6a3cf160ab682e6894746cfb
William Day <williamjday11@gmail.com>
2021-10-12 11:41:41 +0100

    WD: Corrected typo missing ) with previous commit

--- public/javascripts/ability.js
+++ public/javascripts/ability.js
@@ -1,5 +1,5 @@
 class Ability {
-  constructor(name, minAttack = Config.baseMinAttack, maxAttack = Config.baseMaxAttack {
+  constructor(name, minAttack = Config.baseMinAttack, maxAttack = Config.baseMaxAttack) {
     this.name = name
     this.min = minAttack
     this.max = maxAttack
