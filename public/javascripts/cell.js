12bbe11ef65e44be088972a3072cc0803e82244f
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-12 11:44:14 +0100

    AR & AS: Set east and west exits to false for cells on the map x boundary

--- public/javascripts/cell.js
+++ public/javascripts/cell.js
@@ -3,9 +3,16 @@ class Cell {
     this.x = x;
     this.y = y;
     this.wall = wall;
+    this.exits = new Object;
+    this._calculateExits();
   }
 
   isWall() {
     return this.wall;
+  }
+
+  _calculateExits() {
+    if(this.x == 0) { this.exits.west = false };
+    if(this.x == Config.canvasWidth - Config.cellSize) { this.exits.east = false };
   }
 }
