973756955bc871d908aacb2c65cf7e9a7ab69c20
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-12 13:17:52 +0100

    AR & AS: Add cell neighbour function

--- public/javascripts/cell.js
+++ public/javascripts/cell.js
@@ -4,10 +4,35 @@ class Cell {
     this.y = y;
     this.wall = wall;
     this.exits = new Object;
+    this.neighbours = this._calculateNeighbours();
   }
 
   isWall() {
     return this.wall;
+  }
+
+  _calculateNeighbours() {
+    let cellNeighbours = [];
+
+    let west = [this.x - Config.cellSize, this.y];
+    let east = [this.x + Config.cellSize, this.y];
+    let north = [this.y - Config.cellSize, this.x];
+    let south = [this.y + Config.cellSize, this.x];
+    let northwest = [this.x - Config.cellSize, this.y - Config.cellSize];
+    let southwest = [this.x - Config.cellSize, this.y + Config.cellSize];
+    let northeast = [this.x + Config.cellSize, this.y - Config.cellSize];
+    let southeast = [this.x + Config.cellSize, this.y + Config.cellSize];
+
+    [north, east, south, west, northeast,
+    northwest, southeast, southwest].forEach((dir) => {
+      cellNeighbours.push(dir);
+    });
+
+    cellNeighbours = cellNeighbours.filter((dir) => {
+      return (dir[0] >= 0 && dir[1] >= 0 && dir[0] < Config.canvasWidth && dir[1] < Config.canvasHeight);
+    });
+
+    return cellNeighbours;
   }
 
   calculateExits() {
