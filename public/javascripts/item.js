65525e2e1785eb6e1e7ea20801d28cc0c1286392
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-13 21:43:07 +0100

    AS: Final touch-up

--- public/javascripts/item.js
+++ public/javascripts/item.js
@@ -35,6 +35,7 @@ class Item {
   }
 
   show() {
+    // Broken! Debug?
     if (this.available) {
       fill(0, 255, 0)
       circle(this.regionX + Config.cellSize / 2, this.regionY + Config.cellSize / 2, 20)
