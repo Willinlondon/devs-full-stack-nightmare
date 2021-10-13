9d1fd53580fcfcf028aa0f19aa977c229b92912f
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-13 11:09:50 +0100

    AS: Resolve conflicts in config.js

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -16,18 +16,12 @@ class Config {
     static critChance = 0.1;
     static fleeFailureChance = 0.1;
     static defaultEnemyHealth = 80;
-<<<<<<< HEAD
     static mapDimension = 100;
     static NoOfTunnels = 50;
     static maxTunnelLength = 20;
     static regionDivisor = 10; // meaning e.g. 2 horizontally * 2 vertically, = 4 in total
-=======
-	static mapDimension = 10;
-	static NoOfTunnels = 15;
-  	static maxTunnelLength = 6;
     static precisionStrike = new Ability("Precision Strike", 5, 8, 0, 0);
     static wildFlail = new Ability("Wild Flail", 2, 13, 0, 0);
     static heal = new Ability("Recovery", 0, 0, 4, 7);
-    static undefinitedReality = new Ability("Undefined Reality", 5, 8, 0, 0);
->>>>>>> main
+    static undefinedReality = new Ability("Undefined Reality", 5, 8, 0, 0);
 }
