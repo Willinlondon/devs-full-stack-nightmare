c1efc76579d3e03bd2122ad19cfeaeb8080e9e58
William Day <williamjday11@gmail.com>
2021-10-13 17:22:34 +0100

    AR & WD: Update takeTurn function with new enemyAbility argument

--- public/javascripts/battle.js
+++ public/javascripts/battle.js
@@ -5,8 +5,8 @@ class Battle {
     this.log = [];
   }
 
-  takeTurn(playerAbility, flee = false) {
-    let turn = new Turn(this.player1, this.player2, playerAbility, flee);
+  takeTurn(playerAbility, enemyAbility, flee = false) {
+    let turn = new Turn(this.player1, this.player2, playerAbility, enemyAbility, flee);
     this.log.push(turn);
     this.string = turn.outcome();
   }
