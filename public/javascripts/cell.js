89618ae3cc71e2eca2b2b05a8d22ddad09b0ea84
Ahsan <rasul.ahsan@gmail.com>
2021-10-13 11:49:40 +0100

    AR : tried adding to array but didnt work

--- public/javascripts/cell.js
+++ public/javascripts/cell.js
@@ -1,75 +1,75 @@
 class Cell {
-  constructor(x, y, wall) {
-    this.x = x;
-    this.y = y;
-    this.wall = wall;
-    this.exits = new Object;
-    this.constructor.all.push(this);
-    this.region;
-  }
+	constructor(x, y, wall) {
+		this.x = x;
+		this.y = y;
+		this.wall = wall;
+		this.exits = new Object();
+		this.constructor.all.push(this);
+		this.region;
+	}
 
-  static all = [];
+	static all = [];
 
-  static filterByRegion(region) {
-    return Cell.all.filter(cell => cell.region === region)
-  }
+	static filterByRegion(region) {
+		return Cell.all.filter((cell) => cell.region === region);
+	}
 
-  static find(x, y) {
-    return Cell.all.find(cell => cell.x === x && cell.y === y)
-  }
+	static find(x, y) {
+		return Cell.all.find((cell) => cell.x === x && cell.y === y);
+	}
 
-  static all = [];
+	static all = [];
 
-  isWall() {
-    return this.wall;
-  }
+	isWall() {
+		return this.wall;
+	}
 
-  calculateExits() {
-    this.exits.west = this._freeCell([this.x - Config.cellSize, this.y]);
-    this.exits.east = this._freeCell([this.x + Config.cellSize, this.y]);
-    this.exits.north = this._freeCell([this.x, this.y - Config.cellSize]);
-    this.exits.south = this._freeCell([this.x, this.y + Config.cellSize]);
+	calculateExits() {
+		this.exits.west = this._freeCell([this.x - Config.cellSize, this.y]);
+		this.exits.east = this._freeCell([this.x + Config.cellSize, this.y]);
+		this.exits.north = this._freeCell([this.x, this.y - Config.cellSize]);
+		this.exits.south = this._freeCell([this.x, this.y + Config.cellSize]);
 
-    // ANYONE introducing these vars, feel free, but TRIPLE CHECK THE MATHS!!!!!
-    // this.exits.northwest = this._freeCell([this.x - Config.cellSize, this.y - Config.cellSize]);
-    // this.exits.southwest = this._freeCell([this.x - Config.cellSize, this.y + Config.cellSize]);
-    // this.exits.northeast = this._freeCell([this.x + Config.cellSize, this.y - Config.cellSize]);
-    // this.exits.southeast = this._freeCell([this.x + Config.cellSize, this.y + Config.cellSize]);
-  }
+		// ANYONE introducing these vars, feel free, but TRIPLE CHECK THE MATHS!!!!!
+		// this.exits.northwest = this._freeCell([this.x - Config.cellSize, this.y - Config.cellSize]);
+		// this.exits.southwest = this._freeCell([this.x - Config.cellSize, this.y + Config.cellSize]);
+		// this.exits.northeast = this._freeCell([this.x + Config.cellSize, this.y - Config.cellSize]);
+		// this.exits.southeast = this._freeCell([this.x + Config.cellSize, this.y + Config.cellSize]);
+	}
 
-  calculateTile() { 
-    this.tileType = 0; 
-    this.tileType = this.exits.north ? this.tileType + 1 : this.tileType; 
-    this.tileType = this.exits.east ? this.tileType + 2 : this.tileType; 
-    this.tileType = this.exits.south ? this.tileType + 4 : this.tileType; 
-    this.tileType = this.exits.west ? this.tileType + 8 : this.tileType; 
-  }
+	calculateTile() {
+		this.tileType = 0;
+		this.tileType = this.exits.north ? this.tileType + 1 : this.tileType;
+		this.tileType = this.exits.east ? this.tileType + 2 : this.tileType;
+		this.tileType = this.exits.south ? this.tileType + 4 : this.tileType;
+		this.tileType = this.exits.west ? this.tileType + 8 : this.tileType;
+	}
 
-  show() {
-    if (this.isWall()) {
-      image(wallImg, this.regionX, this.regionY);
-    } else {
-      image(tileImg, this.regionX, this.regionY);
-    }
-  }
+	show() {
+		if (this.isWall()) {
+			image(wallImg, this.regionX, this.regionY);
+		} else {
+			image(tileImg, this.regionX, this.regionY);
+		}
+	}
 
+	_freeCell(position) {
+		let x = position[0];
+		let y = position[1];
+		let cell = this._cellAt(position);
 
+		if (cell != undefined) {
+			return !cell.wall;
+		} else {
+			return false;
+		}
+	}
 
-  _freeCell(position) {
-    let x = position[0];
-    let y = position[1];
-    let cell = this._cellAt(position);
+	_cellAt(position) {
+		const result = Cell.all.find(
+			(cell) => cell.x === position[0] && cell.y === position[1]
+		);
 
-    if (cell != undefined) {
-      return !cell.wall
-    } else {
-      return false
-    }
-  }
-
-  _cellAt(position) {
-    const result = Cell.all.find(cell => cell.x === position[0] && cell.y === position[1])
-
-    return result;
-  }
+		return result;
+	}
 }
--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,16 +1,16 @@
 function addToScoreDatabase(username, score) {
-  const gameData = { username, score };
+	const gameData = { username, score };
 
-  fetch('/score', {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    method: 'POST',
-    body: JSON.stringify(gameData),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data.scores));
+	fetch("/score", {
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		method: "POST",
+		body: JSON.stringify(gameData),
+	})
+		.then((response) => response.json())
+		.then((data) => console.log(data.scores));
 }
 
 const game = new Game();
@@ -20,7 +20,7 @@ let healButton;
 let okButton;
 let fleeButton;
 let battleBackgroundImage;
-const battleBackgroundImagePath = './stylesheets/assets/battleBackground.jpg';
+const battleBackgroundImagePath = "./stylesheets/assets/battleBackground.jpg";
 let tileImg;
 let wallImg;
 let playerImg;
@@ -29,132 +29,130 @@ let enemyImg;
 let backgroundMusic;
 
 function preload() {
-  tileImg = loadImage('./images/tile1.png');
-  tileImg.resize(Config.cellSize, Config.cellSize);
-  wallImg = loadImage('./images/wall1.png');
-  wallImg.resize(Config.cellSize, Config.cellSize);
-  playerImg = loadImage('./images/idlePlayer1CROPPED.png');
-  playerImg2 = createImg('./images/playerIdleAnimations.gif');
-  enemyImg = createImg('./images/idleMinotaur.gif', 'enemy');
-  backgroundMusic = loadSound('./stylesheets/assets/map-music-but-quiet.wav');
+	tileImg = loadImage(`./images/new-tiles/1.png`);
+	tileImg.resize(Config.cellSize, Config.cellSize);
+	wallImg = loadImage("./images/wall1.png");
+	wallImg.resize(Config.cellSize, Config.cellSize);
+	playerImg = loadImage("./images/idlePlayer1CROPPED.png");
+	playerImg2 = createImg("./images/playerIdleAnimations.gif");
+	enemyImg = createImg("./images/idleMinotaur.gif", "enemy");
+	backgroundMusic = loadSound("./stylesheets/assets/map-music-but-quiet.wav");
 }
 
 function setup() {
-  createPrecisionStrikeButton();
-  createWildFlailButton();
-  createHealButton();
+	createPrecisionStrikeButton();
+	createWildFlailButton();
+	createHealButton();
 	createOkButton();
 	createFleeButton();
 	canvas = createCanvas(Config.canvasWidth, Config.canvasHeight);
 	canvas.parent("play-area");
-  enemyImg.parent("right");
-  playerImg2.parent("left");
+	enemyImg.parent("right");
+	playerImg2.parent("left");
 	battleBackroundImage = loadImage(battleBackgroundImagePath);
 }
 
 function draw() {
-  background(0);
+	background(0);
 
-  switch (game.state) {
-    case 'mapScreen':
-      //  backgroundMusic.play();
-      enemyImg.hide();
-      playerImg2.show();
-      okButton.hide();
-      precisionStrikeButton.hide();
-      wildFlailButton.hide();
-      healButton.hide();
+	switch (game.state) {
+		case "mapScreen":
+			//  backgroundMusic.play();
+			enemyImg.hide();
+			playerImg2.show();
+			okButton.hide();
+			precisionStrikeButton.hide();
+			wildFlailButton.hide();
+			healButton.hide();
 			fleeButton.hide();
 			game.showMap();
 			playerImg.resize(Config.spriteSize / 2, Config.spriteSize / 2);
 
-      image(
-        playerImg,
-        game.player.gridX + Config.cellSize / 4,
-        game.player.gridY + Config.cellSize / 4
-      );
+			image(
+				playerImg,
+				game.player.gridX + Config.cellSize / 4,
+				game.player.gridY + Config.cellSize / 4
+			);
 			// fill(Config.playerColour);
-      // rect(game.player.location[0],game.player.location[1], Config.spriteSize);
+			// rect(game.player.location[0],game.player.location[1], Config.spriteSize);
 
-      break;
-    case 'battleScreen':
-      background(battleBackroundImage, 0, 0);
-      enemyImg.show();
-      playerImg2.show();
-      game.showBattle();
-      precisionStrikeButton.show();
-      wildFlailButton.show();
-      healButton.show();
+			break;
+		case "battleScreen":
+			background(battleBackroundImage, 0, 0);
+			enemyImg.show();
+			playerImg2.show();
+			game.showBattle();
+			precisionStrikeButton.show();
+			wildFlailButton.show();
+			healButton.show();
 			fleeButton.show();
 			break;
 		case "gameOver":
 			okButton.hide();
-      enemyImg.hide();
-      playerImg2.show();
-      precisionStrikeButton.hide();
-      wildFlailButton.hide();
-      healButton.hide();
+			enemyImg.hide();
+			playerImg2.show();
+			precisionStrikeButton.hide();
+			wildFlailButton.hide();
+			healButton.hide();
 			fleeButton.hide();
 			game.showGameOver();
 			break;
 		case "victoryScreen":
-      background(battleBackroundImage, 0, 0);
-      enemyImg.hide();
-      okButton.show();
-      playerImg2.show();
-      precisionStrikeButton.hide();
-      wildFlailButton.hide();
+			background(battleBackroundImage, 0, 0);
+			enemyImg.hide();
+			okButton.show();
+			playerImg2.show();
+			precisionStrikeButton.hide();
+			wildFlailButton.hide();
 			fleeButton.hide();
-      healButton.hide();
+			healButton.hide();
 			game.showVictoryScreen();
 			break;
 	}
 }
 
 function keyPressed() {
-  if (game.state === 'mapScreen') {
-    if (keyCode === LEFT_ARROW || keyCode === 65) {
-      game.playerAction('west', 75);
-    }
-    if (keyCode === RIGHT_ARROW || keyCode === 68) {
-      game.playerAction('east', 75);
-    }
-    if (keyCode === UP_ARROW || keyCode === 87) {
-      game.playerAction('north', 75);
-    }
-    if (keyCode === DOWN_ARROW || keyCode === 83) {
-      game.playerAction('south', 75);
-    }
-  }
+	if (game.state === "mapScreen") {
+		if (keyCode === LEFT_ARROW || keyCode === 65) {
+			game.playerAction("west", 75);
+		}
+		if (keyCode === RIGHT_ARROW || keyCode === 68) {
+			game.playerAction("east", 75);
+		}
+		if (keyCode === UP_ARROW || keyCode === 87) {
+			game.playerAction("north", 75);
+		}
+		if (keyCode === DOWN_ARROW || keyCode === 83) {
+			game.playerAction("south", 75);
+		}
+	}
 }
 
 function createOkButton() {
-  okButton = createButton('OK');
-  okButton.parent('okButton');
+	okButton = createButton("OK");
+	okButton.parent("okButton");
 
-  okButton.mousePressed(() => {
-    game.battle = null;
-    game.state = 'mapScreen';
-  });
+	okButton.mousePressed(() => {
+		game.battle = null;
+		game.state = "mapScreen";
+	});
 }
 
 function createPrecisionStrikeButton() {
+	precisionStrikeButton = createButton("Precision Strike");
+	precisionStrikeButton.parent("strike");
 
-  precisionStrikeButton = createButton('Precision Strike');
-  precisionStrikeButton.parent('strike');
-
-  precisionStrikeButton.mousePressed(() => {
-    if (game.battle) {
-      game.battle.takeTurn(Ability.find("Precision Strike"));
-    }
-  });
+	precisionStrikeButton.mousePressed(() => {
+		if (game.battle) {
+			game.battle.takeTurn(Ability.find("Precision Strike"));
+		}
+	});
 }
 
 function createWildFlailButton() {
-
 	wildFlailButton = createButton("Wild Flail");
 	//wildFlailButton.position(500, 500);
-  wildFlailButton.parent("wildflail");
+	wildFlailButton.parent("wildflail");
 
 	wildFlailButton.mousePressed(() => {
 		if (game.battle) {
@@ -164,27 +162,27 @@ function createWildFlailButton() {
 }
 
 function createHealButton() {
-  healButton = createButton('Recovery');
-  healButton.parent("heal");
+	healButton = createButton("Recovery");
+	healButton.parent("heal");
 
-  healButton.mousePressed(() => {
+	healButton.mousePressed(() => {
 		if (game.battle) {
-      heal = new Ability("Recovery");
+			heal = new Ability("Recovery");
 			game.battle.takeTurn(heal);
-    }
-  });
+		}
+	});
 }
 
 function createFleeButton() {
-  fleeButton = createButton('Flee!');
-  fleeButton.parent('flee');
+	fleeButton = createButton("Flee!");
+	fleeButton.parent("flee");
 
-  fleeButton.mousePressed(() => {
-    if (Math.random() > Config.fleeFailureChance) {
-      game.battle = null;
-      game.state = 'mapScreen';
-    } else {
-      game.battle.takeTurn('Flee!', true);
-    }
-  });
+	fleeButton.mousePressed(() => {
+		if (Math.random() > Config.fleeFailureChance) {
+			game.battle = null;
+			game.state = "mapScreen";
+		} else {
+			game.battle.takeTurn("Flee!", true);
+		}
+	});
 }
