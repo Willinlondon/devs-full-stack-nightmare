390c726a1ffe3a8fa63e0e0c485191ec9a15b82b
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-14 11:13:22 +0100

    AS: Add score based on battle outcomes in game#showBattle

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -2,7 +2,7 @@ class Config {
 	static canvasWidth = 750;
 	static canvasHeight = 750;
 	static cellSize = 75;
-	static encounterProbability = 1;
+	static encounterProbability = 0.8;
 	static gridSize = 100;
 	static spriteSize = 75;
 	static playerColour = 155;
