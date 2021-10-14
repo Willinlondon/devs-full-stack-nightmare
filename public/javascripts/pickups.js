ab984f5538d398c1d21a0e42981a17e06638ec5e
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-10-14 20:12:57 +0100

    AS: Adjust item/pickup format

---
+++ public/javascripts/pickups.js
@@ -0,0 +1,18 @@
+class Pickups {
+  static all = [
+    function(game, x, y) {
+      let ticketXS = new Item(
+        game, x, y, 0.5,
+        "You attempted up a tiny xs ticket",
+        "Job done! Your health increased by 1",
+        () => { if (game.player.health < game.player.maxHealth) game.player.health += 1 },
+      )
+
+      return ticketXS;
+    }
+  ]
+
+  static sample(game, x, y) {
+    return this.all[Math.floor(Math.random() * this.all.length)](game, x, y)
+  }
+}
