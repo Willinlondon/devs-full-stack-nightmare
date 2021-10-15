d233a5c3587d3b71234312ed5e4ea5bccf177c39
danroche10 <danielroche10@gmail.com>
2021-10-15 09:22:50 +0100

    DR: player-input-max-length

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,41 +1,41 @@
 async function addToScoreDatabase(username, score) {
-	const gameData = { username, score };
+  const gameData = { username, score };
 
-	const response = await fetch("/score", {
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-		method: "POST",
-		body: JSON.stringify(gameData),
-	});
-	const data = await response.json();
+  const response = await fetch('/score', {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(gameData),
+  });
+  const data = await response.json();
   console.log(data);
 }
 
 const game = new Game();
-//Buttons
+// Buttons
 let calculatedProcessButton;
 let stabInTheDarkButton;
 let refreshButton;
 let fleeButton;
 let okButton;
-//Background assets
+// Background assets
 let battleBackgroundImage;
-const battleBackgroundImagePath = "./stylesheets/assets/battleBackground.jpg";
+const battleBackgroundImagePath = './stylesheets/assets/battleBackground.jpg';
 let wallImg;
 let backgroundMusic;
-//Player assets
+// Player assets
 let playerImg;
 let playerImg2;
 let playerFaintAnimation;
-//Enemy assets
+// Enemy assets
 let enemyImg;
 let buggerIdle;
 let buggerFainting;
 let faintingEnemy;
 let Zoomer;
-//Misc assets
+// Misc assets
 let startTime;
 let ghLogo;
 let jasmineLogo;
@@ -44,117 +44,119 @@ let inputPlayerName;
 let beginButton;
 
 function preload() {
-	//Enemy assets
-	enemyImg = createImg("./images/idleMinotaur.gif", "enemy");
-  Zoomer = createImg("./images/Zoomer.gif", "enemy");
-	faintingEnemy = createImg("./images/faintingEnemy.gif", "fainting monster");
-	buggerIdle = createImg("./images/buggerIdle.gif", "enemy");
-	buggerFainting = createImg("./images/buggerFainting.gif", "fainting golem");
+  // Enemy assets
+  enemyImg = createImg('./images/idleMinotaur.gif', 'enemy');
+  Zoomer = createImg('./images/Zoomer.gif', 'enemy');
+  faintingEnemy = createImg('./images/faintingEnemy.gif', 'fainting monster');
+  buggerIdle = createImg('./images/buggerIdle.gif', 'enemy');
+  buggerFainting = createImg('./images/buggerFainting.gif', 'fainting golem');
   jasmineLogo = loadImage('./images/jasmine-logo.png');
   zoomLogo = loadImage('./images/zoom.png');
   ghLogo = loadImage('./images/gh-logo.png');
-	//Background assets
-	backgroundMusic = loadSound("./stylesheets/assets/map-music-but-quiet.wav");
-	tileArray = loadTiles();
-	wallImg = loadImage("./images/wall1.png");
-	wallImg.resize(Config.cellSize, Config.cellSize);
-	ticketImg = loadImage("./images/tickets.png");
-	//Misc assets
-	ghLogo = loadImage("./images/gh-logo.png");
-	//Player assets
-	playerImg = loadImage("./images/idlePlayer1CROPPED.png");
-	playerImg2 = createImg("./images/playerIdleAnimations.gif");
-	playerFaintAnimation = createImg(
-		"./images/playerFaintAnimation.gif",
-		"fainting player"
+  // Background assets
+  backgroundMusic = loadSound('./stylesheets/assets/map-music-but-quiet.wav');
+  tileArray = loadTiles();
+  wallImg = loadImage('./images/wall1.png');
+  wallImg.resize(Config.cellSize, Config.cellSize);
+  ticketImg = loadImage('./images/tickets.png');
+  // Misc assets
+  ghLogo = loadImage('./images/gh-logo.png');
+  // Player assets
+  playerImg = loadImage('./images/idlePlayer1CROPPED.png');
+  playerImg2 = createImg('./images/playerIdleAnimations.gif');
+  playerFaintAnimation = createImg(
+    './images/playerFaintAnimation.gif',
+    'fainting player'
   );
 }
 
 function loadTiles() {
-	const tileArray = [];
+  const tileArray = [];
 
-	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].forEach((i) => {
-		tileArray.push(loadImage(`./images/new-tiles/${i}.png`));
-	});
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].forEach((i) => {
+    tileArray.push(loadImage(`./images/new-tiles/${i}.png`));
+  });
 
-	tileArray.forEach((img) => {
-		img.resize(Config.cellSize, Config.cellSize);
-	});
+  tileArray.forEach((img) => {
+    img.resize(Config.cellSize, Config.cellSize);
+  });
 
-	return tileArray;
+  return tileArray;
 }
 
 function setup() {
-	//Gameplay
-	createLocalDifficulty();
-	createLocalLuck();
-	game.spawnBosses();
-	game.spawnItems();
-	//Buttons
-	createcalculatedProcessButton();
-	createstabInTheDarkButton();
-	createrefreshButton();
-	createOkButton();
+  // Gameplay
+  createLocalDifficulty();
+  createLocalLuck();
+  game.spawnBosses();
+  game.spawnItems();
+  // Buttons
+  createcalculatedProcessButton();
+  createstabInTheDarkButton();
+  createrefreshButton();
+  createOkButton();
   createBeginButton();
-	createFleeButton();
-	createNewGameButton();
-	//Background
-	canvas = createCanvas(Config.canvasWidth, Config.canvasHeight);
-	canvas.parent("play-area");
+  createFleeButton();
+  createNewGameButton();
+  // Background
+  canvas = createCanvas(Config.canvasWidth, Config.canvasHeight);
+  canvas.parent('play-area');
   battleBackroundImage = loadImage(battleBackgroundImagePath);
-  //Enemy assets
-  enemyImg.parent("right");
-  Zoomer.parent("right");
-  faintingEnemy.parent("right");
-  buggerIdle.parent("right");
-	buggerFainting.parent("right");
-  //Player assets
-  playerImg2.parent("left");
-  playerFaintAnimation.parent("left");
-  //Player Name Input
+  // Enemy assets
+  enemyImg.parent('right');
+  Zoomer.parent('right');
+  faintingEnemy.parent('right');
+  buggerIdle.parent('right');
+  buggerFainting.parent('right');
+  // Player assets
+  playerImg2.parent('left');
+  playerFaintAnimation.parent('left');
+  // Player Name Input
   inputPlayerName = createInput();
-  inputPlayerName.parent("play-area")
-  inputPlayerName.position(Config.canvasWidth / 2, Config.canvasWidth / 3 * 2);
+  inputPlayerName.parent('play-area');
+  inputPlayerName.position(
+    Config.canvasWidth / 2,
+    (Config.canvasWidth / 3) * 2
+  );
 
   Cell.all.forEach((cell) => {
     if (cell.boss) {
       cell.bossImg = null;
 
-      switch(cell.boss.name) {
+      switch (cell.boss.name) {
         case 'Git':
           cell.bossImg = ghLogo;
-        break;
+          break;
         case 'Jasmine':
           cell.bossImg = jasmineLogo;
-        break;
+          break;
         case 'Zoomer':
           cell.bossImg = zoomLogo;
-        break;
+          break;
         default:
           cell.bossImg = ghLogo;
-        break;
+          break;
       }
     }
-  })
-
+  });
 }
 
 function draw() {
-	background(0);
+  background(0);
 
-	switch (game.state) {
-		case "mapScreen":
-			//  backgroundMusic.play();
-			game.showMap();
-			enemyDisplayNoBattle();
-			battleButtonsCheck();
-			newGameCheck();
-			playerImg2.show();
-			okButton.hide();
+  switch (game.state) {
+    case 'mapScreen':
+      //  backgroundMusic.play();
+      game.showMap();
+      enemyDisplayNoBattle();
+      battleButtonsCheck();
+      newGameCheck();
+      playerImg2.show();
+      okButton.hide();
       beginButton.hide();
       inputPlayerName.hide();
-			playerFaintAnimation.hide();
-			playerImg.resize(Config.spriteSize / 2, Config.spriteSize / 2);
+      playerFaintAnimation.hide();
+      playerImg.resize(Config.spriteSize / 2, Config.spriteSize / 2);
       ghLogo.resize(Config.spriteSize / 2, Config.spriteSize / 2);
       jasmineLogo.resize(Config.spriteSize / 2, Config.spriteSize / 2);
       zoomLogo.resize(Config.spriteSize / 2, Config.spriteSize / 2);
@@ -182,7 +184,7 @@ function draw() {
             }
           }
         }
-      })
+      });
 
       image(
         playerImg,
@@ -202,17 +204,17 @@ function draw() {
       playerImg2.show();
       playerFaintAnimation.hide();
       break;
-  	case "gameOver":
-			game.showGameOver();
-			enemyDisplayNoBattle();
-			newGameCheck();
-			buttonsNoBattle();
-			okButton.hide();
+    case 'gameOver':
+      game.showGameOver();
+      enemyDisplayNoBattle();
+      newGameCheck();
+      buttonsNoBattle();
+      okButton.hide();
       beginButton.hide();
       inputPlayerName.hide();
-			playerImg2.hide();
-			playerFaintAnimation.show();
-		break;
+      playerImg2.hide();
+      playerFaintAnimation.show();
+      break;
     case 'victoryScreen':
       background(battleBackroundImage, 0, 0);
       battleButtonsCheck();
@@ -250,223 +252,218 @@ function draw() {
 }
 
 function keyPressed() {
-	if (game.state === "mapScreen") {
-		let moved = false;
-		if (keyCode === 65) {
-			game.player.move("west");
-			moved = true;
-		}
-		if (keyCode === 68) {
-			game.player.move("east");
-			moved = true;
-		}
-		if (keyCode === 87) {
-			game.player.move("north");
-			moved = true;
-		}
-		if (keyCode === 83) {
-			game.player.move("south");
-			moved = true;
-		}
+  if (game.state === 'mapScreen') {
+    let moved = false;
+    if (keyCode === 65) {
+      game.player.move('west');
+      moved = true;
+    }
+    if (keyCode === 68) {
+      game.player.move('east');
+      moved = true;
+    }
+    if (keyCode === 87) {
+      game.player.move('north');
+      moved = true;
+    }
+    if (keyCode === 83) {
+      game.player.move('south');
+      moved = true;
+    }
 
     if (moved) {
       if (game.player.cell.boss) {
         if (!game.player.cell.boss.hasFainted()) {
-          game.enterBattle(game.player.cell.boss)
+          game.enterBattle(game.player.cell.boss);
         }
       } else if (game.player.cell.item) {
         game.player.cell.item.pickUp();
-      } else {
-        if (Math.random() > Config.encounterProbability) {
-          let enemy  = NormalEnemies.sample()
-          game.enterBattle(enemy);
-        }
+      } else if (Math.random() > Config.encounterProbability) {
+        const enemy = NormalEnemies.sample();
+        game.enterBattle(enemy);
       }
     }
   }
-
 }
 function createBeginButton() {
-	beginButton = createImg("./images/okButton150px.png");
-	beginButton.parent("okButton");
-	elementHighlight(beginButton);
-	stopElementHighlight(beginButton);
-	beginButton.mousePressed(() => {
-		game.battle = null;
-		game.state = "mapScreen";
+  beginButton = createImg('./images/okButton150px.png');
+  beginButton.parent('okButton');
+  elementHighlight(beginButton);
+  stopElementHighlight(beginButton);
+  beginButton.mousePressed(() => {
+    game.battle = null;
+    game.state = 'mapScreen';
     game.player.name = inputPlayerName.value();
-	});
+  });
 }
 
 function createOkButton() {
-	okButton = createImg("./images/okButton150px.png");
-	okButton.parent("okButton");
-	elementHighlight(okButton);
-	stopElementHighlight(okButton);
-	okButton.mousePressed(() => {
-		game.battle = null;
-		game.state = "mapScreen";
-	});
+  okButton = createImg('./images/okButton150px.png');
+  okButton.parent('okButton');
+  elementHighlight(okButton);
+  stopElementHighlight(okButton);
+  okButton.mousePressed(() => {
+    game.battle = null;
+    game.state = 'mapScreen';
+  });
 }
 
 function createNewGameButton() {
-	newGameButton = createImg("./images/newGame150px.png");
-	newGameButton.parent("okButton");
-	elementHighlight(newGameButton);
-	stopElementHighlight(newGameButton);
-	newGameButton.mousePressed(() => {
-		open("/", "_self");
-	});
+  newGameButton = createImg('./images/newGame150px.png');
+  newGameButton.parent('okButton');
+  elementHighlight(newGameButton);
+  stopElementHighlight(newGameButton);
+  newGameButton.mousePressed(() => {
+    open('/', '_self');
+  });
 }
 
 function createcalculatedProcessButton() {
-	calculatedProcessButton = createImg("./images/calculatedProcess.png");
-	calculatedProcessButton.parent("strike");
-	elementHighlight(calculatedProcessButton);
-	stopElementHighlight(calculatedProcessButton);
-	calculatedProcessButton.mousePressed(() => {
-		startTime = frameCount;
-		if (game.battle) {
-			game.battle.takeTurn(Ability.find("Calculated Process"));
-		}
-	});
+  calculatedProcessButton = createImg('./images/calculatedProcess.png');
+  calculatedProcessButton.parent('strike');
+  elementHighlight(calculatedProcessButton);
+  stopElementHighlight(calculatedProcessButton);
+  calculatedProcessButton.mousePressed(() => {
+    startTime = frameCount;
+    if (game.battle) {
+      game.battle.takeTurn(Ability.find('Calculated Process'));
+    }
+  });
 }
 
 function createstabInTheDarkButton() {
-	stabInTheDarkButton = createImg("./images/stabInTheDark.png");
-	stabInTheDarkButton.parent("wildflail");
-	elementHighlight(stabInTheDarkButton);
-	stopElementHighlight(stabInTheDarkButton);
-	stabInTheDarkButton.mousePressed(() => {
-		startTime = frameCount;
-		if (game.battle) {
-			game.battle.takeTurn(Ability.find("Stab in the Dark"));
-		}
-	});
+  stabInTheDarkButton = createImg('./images/stabInTheDark.png');
+  stabInTheDarkButton.parent('wildflail');
+  elementHighlight(stabInTheDarkButton);
+  stopElementHighlight(stabInTheDarkButton);
+  stabInTheDarkButton.mousePressed(() => {
+    startTime = frameCount;
+    if (game.battle) {
+      game.battle.takeTurn(Ability.find('Stab in the Dark'));
+    }
+  });
 }
 
 function createrefreshButton() {
-	refreshButton = createImg("./images/refresh150px.png");
-	refreshButton.parent("heal");
-	elementHighlight(refreshButton);
-	stopElementHighlight(refreshButton);
-	refreshButton.mousePressed(() => {
-		startTime = frameCount;
-		if (game.battle) {
-			game.battle.takeTurn(Ability.find("Refresh"));
-		}
-	});
+  refreshButton = createImg('./images/refresh150px.png');
+  refreshButton.parent('heal');
+  elementHighlight(refreshButton);
+  stopElementHighlight(refreshButton);
+  refreshButton.mousePressed(() => {
+    startTime = frameCount;
+    if (game.battle) {
+      game.battle.takeTurn(Ability.find('Refresh'));
+    }
+  });
 }
 
 function createFleeButton() {
-	fleeButton = createImg("./images/flee150px.png");
-	fleeButton.parent("flee");
-	elementHighlight(fleeButton);
-	stopElementHighlight(fleeButton);
-	fleeButton.mousePressed(() => {
-		startTime = frameCount;
-		if (Math.random() > Config.fleeFailureChance) {
-			game.battle = null;
-			game.state = "mapScreen";
-		} else {
-			game.battle.takeTurn("Flee!", true);
-		}
-	});
+  fleeButton = createImg('./images/flee150px.png');
+  fleeButton.parent('flee');
+  elementHighlight(fleeButton);
+  stopElementHighlight(fleeButton);
+  fleeButton.mousePressed(() => {
+    startTime = frameCount;
+    if (Math.random() > Config.fleeFailureChance) {
+      game.battle = null;
+      game.state = 'mapScreen';
+    } else {
+      game.battle.takeTurn('Flee!', true);
+    }
+  });
 }
 
 function createLocalDifficulty() {
-	game.cells.forEach((cell) => {
-		cell.localDifficulty = Math.floor(
-			noise(
-				cell.x + Config.difficultyNoiseOffset,
-				cell.y + Config.difficultyNoiseOffset
-			) *
-				Config.noiseScale *
-				Config.noiseRange
-		);
-	});
+  game.cells.forEach((cell) => {
+    cell.localDifficulty = Math.floor(
+      noise(
+        cell.x + Config.difficultyNoiseOffset,
+        cell.y + Config.difficultyNoiseOffset
+      ) *
+        Config.noiseScale *
+        Config.noiseRange
+    );
+  });
 }
 
 function createLocalLuck() {
-	game.cells.forEach((cell) => {
-		cell.localLuck = Math.floor(
-			noise(cell.x + Config.luckNoiseOffset, cell.y + Config.luckNoiseOffset) *
-				Config.noiseScale *
-				Config.noiseRange
-		);
-	});
+  game.cells.forEach((cell) => {
+    cell.localLuck = Math.floor(
+      noise(cell.x + Config.luckNoiseOffset, cell.y + Config.luckNoiseOffset) *
+        Config.noiseScale *
+        Config.noiseRange
+    );
+  });
 }
 
 function elementHighlight(element) {
-	let toChange = element;
-	toChange.mouseOver(changeColor);
-	function changeColor() {
-		toChange.style("background-color: lightgreen");
-	}
+  const toChange = element;
+  toChange.mouseOver(changeColor);
+  function changeColor() {
+    toChange.style('background-color: lightgreen');
+  }
 }
 
 function stopElementHighlight(element) {
-	let toChange = element;
-	toChange.mouseOut(reverseColor);
-	function reverseColor() {
-		toChange.style("background-color: transparent");
-	}
+  const toChange = element;
+  toChange.mouseOut(reverseColor);
+  function reverseColor() {
+    toChange.style('background-color: transparent');
+  }
 }
 
 function enemyDisplayBattle() {
-
-	faintingEnemy.hide();
-	buggerFainting.hide();
-	if (game.battle.player2.name === "Bugger") {
-		buggerIdle.show();
-		Zoomer.hide();
-		enemyImg.hide();
-	} else if (game.battle.player2.name === "Zoomer") {
-		enemyImg.hide();
-		buggerIdle.hide();
-		Zoomer.show();
-	} else {
-		enemyImg.show();
-		buggerIdle.hide();
+  faintingEnemy.hide();
+  buggerFainting.hide();
+  if (game.battle.player2.name === 'Bugger') {
+    buggerIdle.show();
     Zoomer.hide();
-	}
+    enemyImg.hide();
+  } else if (game.battle.player2.name === 'Zoomer') {
+    enemyImg.hide();
+    buggerIdle.hide();
+    Zoomer.show();
+  } else {
+    enemyImg.show();
+    buggerIdle.hide();
+    Zoomer.hide();
+  }
 }
 
 function enemyDisplayNoBattle() {
-	enemyImg.hide();
-	Zoomer.hide();
-	faintingEnemy.hide();
-	buggerFainting.hide();
-	buggerIdle.hide();
+  enemyImg.hide();
+  Zoomer.hide();
+  faintingEnemy.hide();
+  buggerFainting.hide();
+  buggerIdle.hide();
 }
 
 function enemyFainted() {
-	if (game.battle.player2.name === "Bugger") {
-		buggerFainting.show();
-	} else {
-		faintingEnemy.show();
-	}
-
+  if (game.battle.player2.name === 'Bugger') {
+    buggerFainting.show();
+  } else {
+    faintingEnemy.show();
+  }
 }
 
 function battleButtonsCheck() {
-	if (game.state === "battleScreen") {
-		calculatedProcessButton.show();
-		stabInTheDarkButton.show();
-		refreshButton.show();
-		fleeButton.show();
-	} else {
-		calculatedProcessButton.hide();
-		stabInTheDarkButton.hide();
-		refreshButton.hide();
-		fleeButton.hide();
-	}
+  if (game.state === 'battleScreen') {
+    calculatedProcessButton.show();
+    stabInTheDarkButton.show();
+    refreshButton.show();
+    fleeButton.show();
+  } else {
+    calculatedProcessButton.hide();
+    stabInTheDarkButton.hide();
+    refreshButton.hide();
+    fleeButton.hide();
+  }
 }
 
 function newGameCheck() {
-	if (game.state === "gameOver") {
-		newGameButton.show();
-	} else {
-		newGameButton.hide();
-	}
+  if (game.state === 'gameOver') {
+    newGameButton.show();
+  } else {
+    newGameButton.hide();
+  }
 }
